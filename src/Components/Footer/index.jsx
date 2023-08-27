34db1a7427291691b12f27b821ff4f6baa593845
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-27 15:10:19 +0300

    Create Footer

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+
+    <div>
+        <hr />
+        <p className='text-muted'>Dictionary, 2023 designed with &#9829;</p>
+    </div>
+  )
+}
