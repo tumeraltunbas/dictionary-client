84ac7ea1f8d1fb91eff694b335150b9f08023b37
Tümer Altunbaş <tumeraltunbass@gmail.com>
2023-08-27 15:12:11 +0300

    Create Layout

---
+++ src/Components/Layout/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import { Header } from "../Header/Header";
+import { Sidebar } from "../Sidebar/Sidebar";
+import { Outlet } from "react-router-dom";
+import { Footer } from "../Footer/Footer";
+
+export const Layout = () => {
+  return (
+    <>
+      <Container>
+        <Header />
+        <div className="row mt-3">
+          <div className="col-3">
+            <Sidebar />
+          </div>
+          <div className="col-7">
+            <Outlet />
+            <Footer />
+          </div>
+          <div className="col-2">
+            Ad Area
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
